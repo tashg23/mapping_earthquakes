52de0533149f3263a11a2e22546da5f2791d5762
nitashagill23@gmail.com <nitashagill23@gmail.com>
2023-03-12 14:15:12 -0400

    Added geojson file

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,26 +1,54 @@
-// Add console.log to check to see if our code is working 
-console.log('working');
+// Create the map object with a center at the San Francisco Airport
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Create the map object with a center and zoom level 
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"14",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data 
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer); 
+    layer.bindPopup("<h2>" + feature.properties.faa + "</h2> <hr> <h3>" +feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+
+
+
+
+
+// // Grabbing our GeoJSON data 
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map 
+//   pointToLayer: function (feature, latlng) {
+//     console.log(feature); 
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3> " + feature.properties.city + ", " + feature.properties.country + "</h3");
+//   }
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY,
 });
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-// Get data from cities.js
-var cityData = cities;
-
-  // Loop through cities array and create one marker for each city 
-  cityData.forEach(city=>
-    L.circleMarker(city.location, {
-        radius: city.population/100000, 
-        color: "orange"
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map));
